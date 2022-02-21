dee63ac8ca8e864918022e36788a5e6ef86ef38b
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2022-02-21 15:54:27 +0800

    完成导航栏

--- next-youtube/component/Navbar.jsx
+++ next-youtube/component/Navbar.jsx
@@ -1,7 +1,29 @@
+import styles from '../styles/Navbar.module.css';
+
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className={styles.container}>
+      <a href='/about'>Jean</a>
+
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <a href='/products/design'>DESING</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href='/products/development'>DEVELOPMENT</a>
+        </li>
+
+        <li className={styles.listItem}>
+          <a href='/products/production'>PRODUCTION</a>
+        </li>
+
+        <li className={styles.listItem}>
+          <a href='/products/photograhy'>PHOTOGRAPY</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href='/products/contact'>CONTACT</a>
+        </li>
+      </ul>
     </div>
   )
 }
