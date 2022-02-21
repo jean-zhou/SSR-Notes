5a18eb9b5d4961860b0acc9949507bbc78d00f8a
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2022-02-21 14:40:54 +0800

    next 的 动态导入

---
+++ next-youtube/component/loading.js
@@ -0,0 +1,30 @@
+export default function Loading() {
+  return (
+    <div>
+
+      loadingloadingloading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+      loading
+    </div>
+  )
+}
