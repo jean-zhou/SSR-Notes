06609f354283a6d90b2cec0d2addfc3e326779ed
jean-zhou <7774846+jean-zhou@user.noreply.gitee.com>
2022-02-23 17:14:30 +0800

    完成移动端响应式css样式

--- next-youtube/component/Navbar.jsx
+++ next-youtube/component/Navbar.jsx
@@ -1,9 +1,18 @@
 import styles from '../styles/Navbar.module.css';
-
+import Link from 'next/link'
+import { useState } from 'react';
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const toggleHam = () => {
+    // console.log('open', open);
+    setOpen(!open)
+  }
+  const closeMenu = () => {
+    setOpen(false)
+  }
   return (
     <div className={styles.container}>
-      <a href='/about'>Jean</a>
+      <a href='/'>Jean</a>
 
       <ul className={styles.list}>
         <li className={styles.listItem}>
@@ -22,6 +31,32 @@ const Navbar = () => {
         </li>
         <li className={styles.listItem}>
           <a href='/contact'>CONTACT</a>
+        </li>
+      </ul>
+
+      <div className={styles.hamburger} onClick={toggleHam}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+
+      <ul onClick={closeMenu} className={styles.menu} style={{right: open? '0px': '-50vw'}}>
+        <li className={styles.menuItem}>
+          <Link href='/products/design'>DESING</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/products/development'>DEVELOPMENT</Link>
+        </li>
+
+        <li className={styles.menuItem}>
+          <Link href='/products/production'>PRODUCTION</Link>
+        </li>
+
+        <li className={styles.menuItem}>
+          <Link href='/products/photography'>PHOTOGRAPY</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/contact'>CONTACT</Link>
         </li>
       </ul>
     </div>
